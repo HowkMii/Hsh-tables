c76f8a4c4be3197b13a594dcdb158f8dfec71e29
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-09 20:09:52 +0100

    first commit

---
+++ hash tables.py
@@ -0,0 +1,6 @@
+
+
+import numpy as np                # library for array and matrix manipulation
+import pprint                     # utilities for console printing 
+from utils_nb import plot_vectors # helper function to plot vectors
+import matplotlib.pyplot as plt   # visualization library
