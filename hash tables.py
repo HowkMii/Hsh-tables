0fd335397c87bfd938271a30388ff3eb2bcffc41
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-09 20:11:16 +0100

    The hash function is just the remainder of the integer division between each element and the desired number of buckets.

--- hash tables.py
+++ hash tables.py
@@ -4,3 +4,17 @@ import numpy as np                # library for array and matrix manipulation
 import pprint                     # utilities for console printing 
 from utils_nb import plot_vectors # helper function to plot vectors
 import matplotlib.pyplot as plt   # visualization library
+
+
+def basic_hash_table(value_l, n_buckets):
+    
+    def hash_function(value, n_buckets):
+        return int(value) % n_buckets
+    
+    hash_table = {i:[] for i in range(n_buckets)} # Initialize all the buckets in the hash table as empty lists
+
+    for value in value_l:
+        hash_value = hash_function(value,n_buckets) # Get the hash key for the given value
+        hash_table[hash_value].append(value) # Add the element to the corresponding bucket
+    
+    return hash_table
